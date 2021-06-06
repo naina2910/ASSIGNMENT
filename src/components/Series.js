918117dab47ab3259a6a2a82831916697244f90e
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2021-06-06 16:55:56 +0530

    series page completed

--- src/components/Series.js
+++ src/components/Series.js
@@ -1,13 +1,66 @@
 import Title from "./Title";
-import {useEffect,useState} from "react"
+import React,{Component} from "react"
 import MovieCard from "./MovieCard";
+import "../css/movie.css"
 
-
-
-export default function Series(){
-    return(
-        <div>
-            <Title title="Popular Series"/>
-        </div>
-    );
+export default class Series extends Component{
+    constructor(){
+        super();
+        this.state={
+            data:[],
+            error:null,
+            isLoading:true
+        };
+    }
+    componentDidMount(){
+        fetch("./assets/data.json", {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            credentials: "same-origin"
+        })
+        .then(response => {
+            if (response.ok) {
+              return response;
+            } else {
+              var error = new Error('Error ' + response.status + ': ' + response.statusText);
+              error.response = response;
+              throw error;
+            }
+          })
+        .then(response => response.json())
+        .then(response => {
+            this.setState({data:response.entries})
+        })
+        .finally(()=> this.setState({isLoading:false}))
+        .catch(error =>this.setState({error:"Error.."+error.message}));
+    }
+    render(){
+        
+        var items=this.state.data.filter((item)=>{
+           return item.releaseYear>=2010 && item.programType=="series"
+        }).slice(0,21).sort(function(a, b){
+            var x = a.title.toLowerCase();
+            var y = b.title.toLowerCase();
+            if (x < y) {return -1;}
+            if (x > y) {return 1;}
+            return 0;
+          })
+        
+        return(
+            <div>
+                <Title title="Popular Series"/>
+                <div className="moviecontainer">
+                    {
+                        items.map((item)=>{
+                            return(
+                                <MovieCard title={item.title} image={item.images.PosterArt}/>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }  
 }
